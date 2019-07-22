08cd3974688d3ab3b1410e96ed75a1593da803e6
Tom <thomas.berghuis@gmail.com>
2019-07-22 11:59:48 +1000

    .

--- tictactoe-client/src/socket/index.js
+++ tictactoe-client/src/socket/index.js
@@ -1,5 +1,9 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+// TODO https
+// proxy pass /socket.io/
+// const socket = io("http://localhost:3000");
+// TODO env var localhost for dev
+const socket = io("https://tictactoe-socketio.tomberghuis.site");
 
 export default socket;
